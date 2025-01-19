e11dc34f004759a235e1794c53490bba8490898f
jed <jed@ramonaoptics.com>
2025-01-18 20:30:24 -0500

    fix example and rename

--- example.py
+++ example.py
@@ -85,7 +85,7 @@ file_size
 first_tag_offset = first_IFD_offset + 2
 fh.seek(first_tag_offset)
 tag = fh.read(12)
-tag_id_map = {274: 'Orientation', 270: 'ImageDescription', 254: 'NewSubfileType', 256: 'ImageWidth', 257: 'ImageLength', 258: 'BitsPerSample', 259: 'Compression', 262: 'PhotometricInterpretation', 273: 'StripOffsets', 277: 'SamplesPerPixel', 278: 'RowsPerStrip', 279: 'StripByteCounts', 282: 'XResolution', 283: 'YResolution', 284: 'PlanarConfiguration', 296: 'ResolutionUnit', 322: 'TileWidth', 323: 'TileLength', 324: 'TileOffsets', 325: 'TileByteCounts', 338: 'ExtraSamples', 339: 'SampleFormat', 530: 'YCbCrSubSampling', 531: 'YCbCrPositioning', 33421: 'CZ_LSMINFO', 33432: 'CZ_LSMINFO2', 33445: 'CZ_LSMINFO3', 33446: 'CZ_LSMINFO4'}
+tag_id_map = {305: 'Software', 274: 'Orientation', 270: 'ImageDescription', 254: 'NewSubfileType', 256: 'ImageWidth', 257: 'ImageLength', 258: 'BitsPerSample', 259: 'Compression', 262: 'PhotometricInterpretation', 273: 'StripOffsets', 277: 'SamplesPerPixel', 278: 'RowsPerStrip', 279: 'StripByteCounts', 282: 'XResolution', 283: 'YResolution', 284: 'PlanarConfiguration', 296: 'ResolutionUnit', 322: 'TileWidth', 323: 'TileLength', 324: 'TileOffsets', 325: 'TileByteCounts', 338: 'ExtraSamples', 339: 'SampleFormat', 530: 'YCbCrSubSampling', 531: 'YCbCrPositioning', 33421: 'CZ_LSMINFO', 33432: 'CZ_LSMINFO2', 33445: 'CZ_LSMINFO3', 33446: 'CZ_LSMINFO4'}
 tag_id = struct.unpack(byteorder_sym + 'H', tag[:2])[0]
 tag_id_name = tag_id_map[tag_id]
 data_type_map = {1: 'BYTE', 2: 'ASCII', 3: 'SHORT', 4: 'LONG', 5: 'RATIONAL', 6: 'SBYTE', 7: 'UNDEFINED', 8: 'SSHORT', 9: 'SLONG', 10: 'SRATIONAL', 11: 'SINGLE', 12: 'DOUBLE'}
@@ -124,15 +124,17 @@ for i in range(num_dir_entries):
 from pprint import pprint
 pprint(tags)
 
-fh.seek(tags['StripOffsets']['data_offset'])
-strip_offsets = fh.read(tags['StripOffsets']['data_count'] * tags['StripOffsets']['data_type_byte_count'])
-strip_offsets = struct.unpack(byteorder_sym + 'I'*tags['StripOffsets']['data_count'], strip_offsets)
-print(strip_offsets)
+# fh.seek(tags['StripOffsets']['data_offset'])
+# strip_offsets = fh.read(tags['StripOffsets']['data_count'] * tags['StripOffsets']['data_type_byte_count'])
+# strip_offsets = struct.unpack(byteorder_sym + 'I'*tags['StripOffsets']['data_count'], strip_offsets)
+# print(strip_offsets)
+strip_offsets = [tags['StripOffsets']['data_offset']]
 
-fh.seek(tags['StripByteCounts']['data_offset'])
-strip_byte_counts = fh.read(tags['StripByteCounts']['data_count'] * tags['StripByteCounts']['data_type_byte_count'])
-strip_byte_counts = struct.unpack(byteorder_sym + 'I'*tags['StripByteCounts']['data_count'], strip_byte_counts)
-print(strip_byte_counts)
+# fh.seek(tags['StripByteCounts']['data_offset'])
+# strip_byte_counts = fh.read(tags['StripByteCounts']['data_count'] * tags['StripByteCounts']['data_type_byte_count'])
+# strip_byte_counts = struct.unpack(byteorder_sym + 'I'*tags['StripByteCounts']['data_count'], strip_byte_counts)
+# print(strip_byte_counts)
+strip_byte_counts = [tags['StripByteCounts']['data_offset']]
 
 # compression is short enough to be stored in the tag
 compression = tags['Compression']['data_offset']
